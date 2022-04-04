013507436d29d8f106b66f3025a4fb20753b8cf2
glymphie <glymphie@protonmail.com>
2022-04-04 20:33:40 +0200

    removed READMEs

--- update.sh
+++ update.sh
@@ -3,9 +3,9 @@ rm -rf dmenu dwm dwmblocks st libxft-bgra slock
 
 SUCKLESS="$GIT/suckless"
 
-rsync -a --delete --exclude=".git" "$SUCKLESS/st" .
-rsync -a --delete --exclude=".git" "$SUCKLESS/dmenu" .
-rsync -a --delete --exclude=".git" "$SUCKLESS/dwm" .
-rsync -a --delete --exclude=".git" "$SUCKLESS/libxft-bgra" .
-rsync -a --delete --exclude=".git" "$SUCKLESS/slock" .
-rsync -a --delete --exclude=".git" "$SUCKLESS/dwmblocks" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/st" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/dmenu" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/dwm" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/libxft-bgra" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/slock" .
+rsync -a --delete --exclude=".git" --exclude="README*" "$SUCKLESS/dwmblocks" .
